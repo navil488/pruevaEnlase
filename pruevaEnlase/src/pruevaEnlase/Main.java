ec197448bb65a62219bad7e9b30b873f2ce6f8ba
navil488 <109832610+navil488@users.noreply.github.com>
2024-08-29 22:25:22 -0600

    confirmar

--- pruevaEnlase/src/pruevaEnlase/Main.java
+++ pruevaEnlase/src/pruevaEnlase/Main.java
@@ -1,6 +1,18 @@
 package pruevaEnlase;
 
 public class Main {
+public static void main(String[] args) {
+       
+        Persona persona1 = new Persona("Juan", 25);
+        Persona persona2 = new Persona("Ana", 30);
 
+      
+        persona1.mostrarInfo();
+        persona2.mostrarInfo();
+
+       
+        persona1.setEdad(26);
+        System.out.println("Nueva edad de " + persona1.getNombre() + ": " + persona1.getEdad());
+    }
   
 }
