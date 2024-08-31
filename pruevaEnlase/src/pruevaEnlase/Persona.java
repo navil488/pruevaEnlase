254ddb10cec0b7266b73303b698c4f8415b0d4d4
JGomezM46 <90494585+JGomezM46@users.noreply.github.com>
2024-08-30 20:59:13 -0600

    Update Persona.java

--- pruevaEnlase/src/pruevaEnlase/Persona.java
+++ pruevaEnlase/src/pruevaEnlase/Persona.java
@@ -1,5 +1,38 @@
 package pruevaEnlase;
 
 public class Persona {
+ private String nombre;
+    private int edad;
+
+    
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public int getEdad() {
+        return edad;
+    }
+
+    
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+
+    public void mostrarInfo() {
+        System.out.println("Nombre: " + nombre + ", Edad: " + edad);
+    }
 
 }
